d89108b7d9d95289876b3b849cffe3748bcfb3c8
Austin <acm0063@mix.wvu.edu>
2022-01-30 16:11:55 -0500

    Added Caoursel and Card Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
 import { TopNavBarComponent } from './header/topnavbar.component';
+import { CategoryCardComponent } from './store/category.card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    CarouselComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule
