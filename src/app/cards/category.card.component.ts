ef8eb1e6456f5fc268a4a462cb48a3f40a2aac15
Austin <acm0063@mix.wvu.edu>
2022-02-14 12:47:18 -0500

    Changed index name from store to cards

---
+++ src/app/cards/category.card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'fm-category-card',
+    templateUrl: 'category.card.component.html',
+    styleUrls: ['category.card.component.css']
+})
+export class CategoryCardComponent{
+    @Input() img: string;
+    @Input() name: string;
+    @Input()description: string;
+
+    constructor(){
+        this.img = "";
+        this.name = "missing name";
+        this.description = "Missing description";
+    }
+}
